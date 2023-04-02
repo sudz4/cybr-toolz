f07f3a93b14ba45067c5dfb5eae595ff44c7c611
sudz4 <mwsuther@syr.edu>
2023-04-02 15:24:20 -0700

    hello?

--- main_machine_recon.py
+++ main_machine_recon.py
@@ -7,7 +7,7 @@ import netifaces
 import urllib.request
 import psutil
 
-# GET hostname and local IP addresss
+# GET hostname and local IP addressssssssss
 hostname = socket.gethostname()
 local_ip = socket.gethostbyname(hostname)
 
