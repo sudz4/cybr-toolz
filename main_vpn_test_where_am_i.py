be50a13eaecf825547ac5148884d8df0e14e062d
sudz4 <mwsuther@syr.edu>
2023-04-04 22:06:05 -0400

    dragon comments

--- main_vpn_test_where_am_i.py
+++ main_vpn_test_where_am_i.py
@@ -23,3 +23,15 @@ print("City:", data["city"])
 print("Latitude:", data["latitude"])
 print("Longitude:", data["longitude"])
 print("Time zone:", data["timezone"])
+
+# error handling
+"""
+Merge (default): This option creates a merge commit when pulling changes from the remote repository. 
+
+To set the default preference for all repositories, you can replace git config with git config --global. 
+Once you have configured the desired merge strategy, you can perform a git pull to update your local branch 
+with the changes from the remote repository.
+
+git config pull.rebase false
+git pull
+"""
